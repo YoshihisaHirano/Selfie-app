0fbaf128334797f4756a9368287a78822812f08c
YoshihisaHirano <anastasia7febr@gmail.com>
2020-04-28 21:31:57 +0300

    Add NeDB database functionality

--- public/sketch.js
+++ public/sketch.js
@@ -1,9 +1,7 @@
 //Adding a map to the page
-const mymap = L.map('mapid').setView([0, 0], 2);
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileUrl, { attribution });
-tiles.addTo(mymap);
+let firstTime = true;
 
 //Marker image
 const myicon = L.icon({
@@ -15,19 +13,21 @@ const button = document.getElementById('geolocate');
 //Working with actual geolocation data of the client
 button.addEventListener("click", () => {
 if('geolocation' in navigator) {
-  console.log("geolocation available");
   navigator.geolocation.getCurrentPosition(async (position) => {
-  console.log(position);
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   document.getElementById('lat').textContent = lat;
   document.getElementById('lon').textContent = lon;
-  //Adding actual coordinates to the map
+  //Adding map with actual coordinates
+  if(firstTime) {
+  const mymap = L.map('mapid').setView([lat, lon], 12);
+  const tiles = L.tileLayer(tileUrl, { attribution });
+  tiles.addTo(mymap);
   const marker = L.marker([lat, lon], { icon: myicon }).addTo(mymap);
-  mymap.setView([lat, lon], 12);
-
+}
 //Making a POST request to the server
-  const data = { lat, lon };
+  const keyword = document.getElementById('input').value;
+  const data = { lat, lon, keyword };
   const options = {
     method: 'POST',
     body: JSON.stringify(data),
@@ -39,7 +39,7 @@ if('geolocation' in navigator) {
     const resp = await fetch('/api', options);
     const datum = await resp.json();
     console.log(datum);
-
+    firstTime = false;
 });
 } else {
   console.log('nope');
